61cf54e7ee91814fdec29a048f7f3dbe2d8ed719
Kishore Shiraguppi <kishor4u@gmail.com>
2022-04-16 19:38:12 -0400

    https

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=20b9c3b9";
+const API_URL = "https://www.omdbapi.com?apikey=20b9c3b9";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
